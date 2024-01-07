22eed0d0f9ebba2e61d5303d91d22f8078835316
DragonRider8128 <76656329+DragonRider8128@users.noreply.github.com>
2024-01-07 14:44:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,70 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+import imdb
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+movie_access = imdb.Cinemagoer()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  if request.method == "POST":
+    search = request.form["search"]
+
+    if request.form['submit_btn'] == "movie":   
+      return redirect(url_for('search', name=search))
+    elif request.form['submit_btn'] == 'series':
+      return redirect(url_for('series', name=search))
+
+  return render_template('home.html')
+
+ 
+@app.route('/search',methods=['POST','GET'])
+def search():
+  if request.method == "POST":
+    search = request.form["search"]
+    
+    if request.form['submit_btn'] == "movie":   
+      return redirect(url_for('search', name=search))
+    elif request.form['submit_btn'] == 'series':
+      return redirect(url_for('series', name=search))
+  
+  try:
+    name = request.args['name']
+    results = movie_access.search_movie(name)
+    movies = []
+
+    for i in range(5):
+      id = results[i].movieID
+      movies.append("https://multiembed.mov/directstream.php?video_id=tt" + str(id))
+      
+    
+    return render_template('search.html',results=movies,search=name)
+  except:
+    return redirect('/')
+
+@app.route('/series',methods=['POST','GET'])
+def series():
+  if request.method == "POST":
+    search = request.form["search"]
+
+    if request.form['submit_btn'] == "movie":   
+      return redirect(url_for('search', name=search))
+    elif request.form['submit_btn'] == 'series':
+      return redirect(url_for('series', name=search))
+
+  try:
+    name = request.args['name']
+    results = movie_access.search_movie(name)
+    series = []
+
+    for i in range(5):
+      id = results[i].movieID
+      series.append("https://vidsrc.xyz/embed/tv/tt" + str(id))
+
+
+    return render_template('search.html',results=series,search=name)
+  except:
+    return redirect('/')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=81, debug=True)
